706e59b4e0c313f2cc4296230440a38c6cb35dd5
Raoul Bourquin <raoul.bourquin@sam.math.ethz.ch>
2016-09-26 18:31:38 +0200

    Build subindex pages

--- website/build.py
+++ website/build.py
@@ -14,27 +14,30 @@ def get_rulename(rule):
     return "Q[{}]".format(", ".join(map(str, rule)))
 
 
+def get_rulestr(rule):
+    return "_".join(map(str, rule))
+
+
 def get_filenames(rule):
-    rulestr = "_".join(map(str, rule))
+    rulestr = get_rulestr(rule)
     ruledatafile = "rule_herm_{}.dat".format(rulestr)
     rulenodesplot = "rule_herm_{}_nodes_weights_cp_{}.png".format(rulestr, rulestr)
     ruleweightsplot = "rule_herm_{}_nodes_weights_{}.png".format(rulestr, rulestr)
     ruleweightslogplot = "rule_herm_{}_nodes_weights_log_{}.png".format(rulestr, rulestr)
 
-    return (rulestr,
-            ruledatafile,
+    return (ruledatafile,
             rulenodesplot,
             ruleweightsplot,
             ruleweightslogplot)
-
 
 
 def generate_rulepage(T, rule, polynomialname):
 
     rulename = get_rulename(rule)
 
-    (rulestr,
-     ruledatafile,
+    rulestr = get_rulestr(rule)
+
+    (ruledatafile,
      rulenodesplot,
      ruleweightsplot,
      ruleweightslogplot) = get_filenames(rule)
@@ -60,14 +63,12 @@ def generate_rulepage(T, rule, polynomialname):
                     ruleweightsplot=path.join(datapath, ruleweightsplot),
                     ruleweightslogplot=path.join(datapath, ruleweightslogplot),
                     nodeballs=allroots[0],
-                    weightballs=allweights[0]
-    )
+                    weightballs=allweights[0])
 
     rulepagedstpath = path.join(sitedstpath, polytype, 'rules')
 
     with open(path.join(rulepagedstpath, 'rule_{}.html'.format(rulestr)), 'w') as f:
         f.write(site)
-
 
 
 def generate_pages(polytype, polynomialname):
@@ -77,29 +78,43 @@ def generate_pages(polytype, polynomialname):
     with open(path.join(sitesrcpath, 'rule.html.j2'), 'r') as f:
         T = Template(f.read())
 
-    for rule in list(RI)[:10]:
+    for rule in list(RI):
         print(rule)
         generate_rulepage(T, rule, polynomialname)
 
 
+def generate_subindex(polytype, polynomialname):
 
+    RI = read_rules_inventory(ruledatasrcpath)
+    RI = sorted(list(map(lambda l: list(map(int, l)), RI)))
 
+    rulenames = list(map(get_rulename, RI))
 
+    pages = [path.join(sitedstpath,
+                       polytype,
+                       'rules',
+                       'rule_{}.html'.format("_".join(map(str, rule)))) for rule in RI]
 
+    with open(path.join(sitesrcpath, 'subindex.html.j2'), 'r') as f:
+        T = Template(f.read())
 
+    subindex = T.render(polynomialname=polynomialname,
+                        rules=zip(rulenames, pages))
 
+    subindexpagedstpath = path.join(sitedstpath, polytype)
 
-
+    with open(path.join(subindexpagedstpath, 'subindex.html'), 'w') as f:
+        f.write(subindex)
 
 
 if __name__ == '__main__':
 
     import os
 
-
+    siteurl = '/u/raoulb'
 
     sitesrcpath = '/u/raoulb/rulerepo/website'
-    sitedstpath = '/u/raoulb/ruleweb'
+    sitedstpath = path.join(siteurl, 'ruleweb')
 
     ruledatasrcpath = '/userdata/raoulb/KronrodExtensions/Kronrod_Extensions_Hermite'
 
@@ -113,3 +128,5 @@ if __name__ == '__main__':
         os.makedirs(dd)
 
     generate_pages(polytype, polynomialname)
+
+    generate_subindex(polytype, polynomialname)
